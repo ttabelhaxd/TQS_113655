db3dd007b53981e9b7d9e88ba583521f13a78831
Abel Teixeira <120480217+ttabelhaxd@users.noreply.github.com>
2025-02-18 10:35:30 +0000

    aula1

---
+++ lab1/lab1_2/src/test/java/tqs/sets/BoundedSetOfNaturalsTest.java
@@ -0,0 +1,57 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tqs.sets;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+import tqs.sets.BoundedSetOfNaturals;
+
+/**
+ * @author ico0
+ */
+class BoundedSetOfNaturalsTest {
+    private BoundedSetOfNaturals setA;
+    private BoundedSetOfNaturals setB;
+    private BoundedSetOfNaturals setC;
+
+
+    @BeforeEach
+    public void setUp() {
+        setA = new BoundedSetOfNaturals(1);
+        setB = BoundedSetOfNaturals.fromArray(new int[]{10, 20, 30, 40, 50, 60});
+        setC = BoundedSetOfNaturals.fromArray(new int[]{50, 60});
+    }
+
+    @AfterEach
+    public void tearDown() {
+        setA = setB = setC = null;
+    }
+
+    @Test
+    public void testAddElement() {
+
+        setA.add(99);
+        assertTrue(setA.contains(99), "add: added element not found in set.");
+        assertEquals(1, setA.size());
+
+        assertThrows(IllegalArgumentException.class, () -> setB.add(11), "adding to a full set should throw exception");
+    }
+
+    @Test
+    public void testAddFromBadArray() {
+        int[] elems = new int[]{10, -20, -30};
+
+        // must fail with exception
+        assertThrows(IllegalArgumentException.class, () -> setA.add(elems));
+    }
+
+    @Test
+    public void testIntersects() {
+        assertTrue(setB.intersects(setC), "sets should intersect");
+        assertFalse(setA.intersects(setB), "sets should not intersect");
+    }
+
+}
